5617b4f34d439e3ffe46b5737a54e1331edc9420
1Nelsonel <nelsonmokoro37@gmail.com>
2022-10-20 08:02:01 +0300

    success

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
-            <Route path='/product/:id/' element={<ProductScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
 
           </Routes>
         </Container>
