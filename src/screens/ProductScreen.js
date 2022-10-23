6261d3fc4948347605261ce3a067bb5c2efb2a04
1Nelsonel <nelsonmokoro37@gmail.com>
2022-10-23 09:11:29 +0300

    success

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, ListGroupItem, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-const ProductScreen = () => {
-    const { id } = useParams()
-    const product = products.find(p => p._id === id)
+
+
+function ProductScreen({  }) {
+    const [product, setProduct] = useState([])
+    const { id } = useParams();
+
+    useEffect(() => {
+        async function fetchProduct() {
+            const { data } = await axios.get(`/api/products/${id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+
+    }, [])
+
+
+
+
 
     return (
         <div>
